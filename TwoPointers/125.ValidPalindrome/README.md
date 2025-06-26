5611ec9b491a88284de645d12da95a2160eccc26
llLucidll <akhil10831@gmail.com>
2025-06-26 13:10:28 -0600

    Solved ValidPalindromes

---
+++ TwoPointers/125.ValidPalindrome/README.md
@@ -0,0 +1,9 @@
+**Approach**
+
+
+1. We have to skip past any non alphanumeric characters. For this we can write a function called isalphanum() that can be called to check if the current index is alphanum
+    1. We do this by checking if the character lies between a-z or A-Z or 0-9
+
+2. We use a classic two pointer approach and keep a while loop going as long as l < r
+3. If we find its not a palindrome at any point we break. If its non alphanumeric, we skip that specific pointer forward.
+4. Advance the pointers towards each other if all conditions are fufilled.
---
+++ TwoPointers/125.ValidPalindrome/ValidPalindrome.py
@@ -0,0 +1,27 @@
+def isPalindrome(self, s: str) -> bool:
+    l = 0
+    r = len(s)
+
+    while l < r:
+        while l < r and not alphanum(s[l]):
+            l += 1
+        while r > l and not alphanum(s[r]):
+            r -= 1
+        
+        if s[l].islower() != s[r].islower():
+            return False
+        
+        else:
+            l += 1
+            r -= 1
+    
+    return True
+    
+
+def alphanum(s: str) -> bool:
+    if (ord('a') <= ord(s) <= ord('z') or
+        ord('A') <= ord(s) <= ord('Z') or
+        ord('0') <= ord(s) <= ord('9')):
+        return True
+    else:
+        return False
