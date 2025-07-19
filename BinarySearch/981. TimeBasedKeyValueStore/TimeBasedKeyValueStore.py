53368edbd1779a1cd12f7db59a4d9805c7d122c1
llLucidll <akhil10831@gmail.com>
2025-07-18 21:46:48 -0600

    Solved TimeBasedKeyValueStore

--- BinarySearch/981. TimeBasedKeyValueStore/TimeBasedKeyValueStore.py
+++ BinarySearch/981. TimeBasedKeyValueStore/TimeBasedKeyValueStore.py
@@ -1,6 +1,23 @@
 class TimeMap:
     def __init__(self):
         self.cache = {}
-    def set(key: str, value: str, timestamp: int) -> None:
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.cache:
+            self.cache[key] = []
+        self.cache[key].append([value, timestamp])
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.cache.get(key, [])
 
-    def get(key: str, timestamp: int) -> str:
+        l, r = 0, len(values) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if self.cache[m][1] <= timestamp:
+                res = self.cache[m][0]
+                l = m + 1
+            else:
+                r = r - 1
+        return res
+
